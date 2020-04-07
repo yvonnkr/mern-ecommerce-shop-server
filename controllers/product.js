e36ddca93c5d0dfd6a33e6530307f53d5d4471d4
Yvonne Nkr <yvonnkr86@gmail.com>
2020-04-07 20:59:23 +0100

    read,update,delete routes & controller

--- controllers/product.js
+++ controllers/product.js
@@ -5,6 +5,26 @@ const fs = require("fs");
 const Product = require("../models/product");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
+//middleware
+exports.productById = (req, res, next, id) => {
+  Product.findById(id)
+    .populate("category")
+    .exec((err, product) => {
+      if (err || !product) {
+        return res.status(400).json({
+          error: "Product not found",
+        });
+      }
+      req.product = product;
+      next();
+    });
+};
+
+exports.read = (req, res) => {
+  req.product.photo = undefined;
+  return res.json(req.product);
+};
+
 exports.create = (req, res) => {
   //   let form = new formidable.IncomingForm(); //throws err
   //   form.keepExtensions = true;//throws err
@@ -55,6 +75,59 @@ exports.create = (req, res) => {
         });
       }
       res.json(result);
+    });
+  });
+};
+
+exports.update = (req, res) => {
+  //   let form = new formidable.IncomingForm();
+  //   form.keepExtensions = true;
+
+  let form = new formidable();
+  form.keepExtensions = false;
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Image could not be uploaded",
+      });
+    }
+
+    let product = req.product;
+    product = _.extend(product, fields); //lodash
+
+    //  1kb = 10001 mb = 1000000
+    if (files.photo) {
+      if (files.photo.size > 1000000) {
+        return res.status(400).json({
+          error: "Image should be less than 1mb in size",
+        });
+      }
+      product.photo.data = fs.readFileSync(files.photo.path);
+      product.photo.contentType = files.photo.type;
+    }
+
+    product.save((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err),
+        });
+      }
+      res.json(result);
+    });
+  });
+};
+
+exports.remove = (req, res) => {
+  let product = req.product;
+  product.remove((err, deletedProduct) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    res.json({
+      message: "Product deleted successfully",
     });
   });
 };
--- routes/product.js
+++ routes/product.js
@@ -3,10 +3,35 @@ const router = express.Router();
 
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
-const { create } = require("../controllers/product");
+const {
+  create,
+  productById,
+  read,
+  update,
+  remove,
+} = require("../controllers/product");
+
+router.get("/product/:productId", read);
 
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
 
+router.put(
+  "/product/:productId/:userId",
+  requireSignin,
+  isAuth,
+  isAdmin,
+  update
+);
+
+router.delete(
+  "/product/:productId/:userId",
+  requireSignin,
+  isAuth,
+  isAdmin,
+  remove
+);
+
 router.param("userId", userById);
+router.param("productId", productById);
 
 module.exports = router;
