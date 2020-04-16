a16ed6572c5bec23f334da1c93304446b701efaa
Yvonne Nkr <yvonnkr86@gmail.com>
2020-04-16 23:18:37 +0100

    PORT

--- app.js
+++ app.js
@@ -38,8 +38,8 @@ app.use("/api", productRoutes);
 app.use("/api", braintreeRoutes);
 app.use("/api", orderRoutes);
 
-const port = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
